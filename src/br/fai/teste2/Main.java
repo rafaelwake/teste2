e18ff21dfd3aac3971ed1520945488a2fba727d4
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:20:30 -0300

    firts update

---
+++ src/br/fai/teste2/Main.java
@@ -0,0 +1,15 @@
+package br.fai.teste2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main app = new Main();
+		app.start();
+	}
+	
+	private void start () {
+		
+	}
+
+}
