aa3e52a5ee17d50fc9a3d2e79531a897cc19f226
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:33:45 -0300

    firts dev1

--- src/br/fai/teste2/Main.java
+++ src/br/fai/teste2/Main.java
@@ -1,5 +1,7 @@
 package br.fai.teste2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,9 +9,47 @@ public class Main {
 		Main app = new Main();
 		app.start();
 	}
+	Scanner scanner = new Scanner(System.in);
 	
 	private void start () {
+		usandoWhile();
+		usandoDoWhile();
+		usandoFor();
 		
+		
+	}
+	
+	private void usandoWhile() {
+		System.out.println("usando while");
+		System.out.print("Digite a idade: ");
+		int i = 0;
+		int idade = scanner.nextInt();
+		
+		while(i<30) {
+			System.out.println("idade somando ate 30= "+ idade);
+			idade++;
+			i++;
+		}
+	}
+	
+	private void usandoDoWhile() {
+		System.out.println("usando DO while");
+		int i = 10;
+			
+		while(i>0) {
+			System.out.println("conter= "+ i);
+			i--;
+		}
+	}
+	
+	private void usandoFor() {
+		System.out.println("usando For");
+		int i = 0;
+			
+		while(i<5) {
+			System.out.println("conter= "+ i);
+			i++;
+		}
 	}
 
 }
